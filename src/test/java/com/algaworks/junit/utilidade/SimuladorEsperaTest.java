fb193f4a9ddb0f6b230a278d7b1fb924687be545
xfire741 <eduardovictor28@gmail.com>
2023-02-13 22:03:03 -0300

    Desabilitando Testes unitários e com variáveis de ambiente

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -1,7 +1,10 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import java.time.Duration;
 
@@ -10,7 +13,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class SimuladorEsperaTest {
 
     @Test
+    //@Disabled("Não é mais aplicável")
+    //@EnabledIfEnvironmentVariable(named = "ENV", matches = "PROD")
     void deveEsperarENaoDarTimeout() {
+        //Assumptions.assumeTrue("PROD".equals(System.getenv("ENV")), ()-> "Abortando teste: Não deve ser executado em PROD");
         //assertTimeout(Duration.ofSeconds(1), ()-> SimuladorEspera.esperar(Duration.ofSeconds(10)));
         assertTimeoutPreemptively(Duration.ofSeconds(1), ()-> SimuladorEspera.esperar(Duration.ofSeconds(10)));
     }
