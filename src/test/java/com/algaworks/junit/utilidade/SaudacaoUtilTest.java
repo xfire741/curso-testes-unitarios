dc951b03c7c8c291b69b29755db32fbba0a864f2
xfire741 <eduardovictor28@gmail.com>
2023-03-24 11:51:01 -0300

    Arrange, Act e Assert

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,16 +9,27 @@ class SaudacaoUtilTest {
 
     @Test
     public void saudarTest() {
+        //Arrange
+        int horaValida = 9;
 
-        String saudacao = SaudacaoUtil.saudar(9);
+        //Act
+        String saudacao = SaudacaoUtil.saudar(horaValida);
+
+        //Assert
         assertEquals("Bom dia", saudacao, "Saudação incorreta");
     }
 
     @Test
     public void deveLancarException() {
+        //Arrange
+        int  horaInvalida = -10;
 
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
-                () -> SaudacaoUtil.saudar(-10));
+        //Act
+        Executable executable = () -> SaudacaoUtil.saudar(horaInvalida);
+
+        //Assert
+        IllegalArgumentException illegalArgumentException = assertThrows(
+                IllegalArgumentException.class, executable);
 
         assertEquals("Hora inválida", illegalArgumentException.getMessage());
     }
